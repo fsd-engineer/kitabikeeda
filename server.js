37d2040a4ccc02c82834ec20c9bc2b6b7edda625
anandkr47 <anandkumar.trident@gmail.com>
2023-10-05 23:32:16 +0530

    server js file is modified

--- server.js
+++ server.js
@@ -5,41 +5,48 @@ import morgan from "morgan";
 import connectDB from "./config/db.js";
 import authRoute from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
-import ProductRoutes from "./routes/productRoute.js"
+import ProductRoutes from "./routes/productRoute.js";
 import cors from "cors";
-import  Path  from "path";
-import {fileURLToPath} from "url";
-//configure.env
+import path from "path"; // Change Path to path
+import { fileURLToPath } from "url";
+
+// Configure .env
 dotenv.config();
 
-//database config
+// Database config
 connectDB();
-//esmodule fix
-const __filename=fileURLToPath(import.meta.url);
-const __dirname= path.dirname(__filename);
-//rest object
+
+// ESM module fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// REST object
 const app = express();
 
-//middlewares
+// Middlewares
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname, "./client/build")))
-//routes
+
+// Serve static files
+app.use(express.static(path.join(__dirname, "client", "build")));
+
+// Routes
 app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", ProductRoutes);
 
-//rest api
-app.use("*",function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// REST API
+app.use("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
-//Port
+
+// Port
 const PORT = process.env.PORT || 8000;
-//run listen
+
+// Run listen
 app.listen(PORT, () => {
   console.log(
-    `serving Running on ${process.env.DEV_MODE}mode on port ${PORT}`.bgCyan
-      .white
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.bgCyan.white
   );
 });
